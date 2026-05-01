e762417037099578b3702c22262841bf9e4e94a9
somafu <omafuoloche4@gmail.com>
2026-05-01 13:33:06 +0100

    loadbanner rebuild

--- ASCII/LoadBanner.go
+++ ASCII/LoadBanner.go
@@ -1,34 +1,57 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"errors"
 	"os"
+	"strings"
 )
 
-func LoadBanner(bannerFont string) []string {
+func Load_Banner(bannerFont string) (map[rune][]string, error) {
 
-	file, err := os.Open(bannerFont)
+	file, err := os.ReadFile(bannerFont)
 
 	if err != nil {
-		fmt.Println("Error opening file")
+		return nil, errors.New("Error reading file")
+
 	}
 
-	defer file.Close()
+	text := strings.Split(string(file), "\n")
 
-	scanner := bufio.NewScanner(file)
+	text = text[1:]
 
-	var lines []string
+	var banner = make(map[rune][]string)
 
-	for scanner.Scan() {
-
-		lines = append(lines, scanner.Text())
+	for ch := ' '; ch <= '~'; ch++ {
+		start := ch - 32
+		end := start + 8
+		banner[ch] = text[start:end]
 	}
-
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("Error Scanner file")
-	}
-
-	return lines
+	return banner, nil
 }
+
+// func LoadBanner(bannerFont string) []string {
+
+// 	file, err := os.Open(bannerFont)
+
+// 	if err != nil {
+// 		fmt.Println("Error opening file")
+// 	}
+
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+
+// 	var lines []string
+
+// 	for scanner.Scan() {
+
+// 		lines = append(lines, scanner.Text())
+// 	}
+
+// 	err = scanner.Err()
+// 	if err != nil {
+// 		fmt.Println("Error Scanner file")
+// 	}
+
+// 	return lines
+// }
