a0a06d84536b4f207456403cba94547859b76b3d
somafu <omafuoloche4@gmail.com>
2026-05-03 06:00:53 +0100

    file system

---
+++ week_2_practice/file system/fs_create.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Create file
+func Createfile() {
+
+	// this function create a file Myname.txt and write a text to that same file
+	newfile, err := os.Create("Myname.txt")
+
+	if err != nil {
+		fmt.Println("error creating file")
+		return
+	}
+	defer newfile.Close()
+
+	fmt.Print("Write your full name: ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+
+	myname := scanner.Text()
+
+	err = os.WriteFile("Myname.txt", []byte(myname), 0644)
+
+	if err != nil {
+		fmt.Println("error writing file", err)
+		return
+	}
+}
+
+// this function rename file
+func renamefile() {
+
+	file, err := os.Create("temp.txt")
+
+	if err != nil {
+		fmt.Println("error creating file")
+
+		return
+	}
+	defer file.Close()
+
+	err = os.Rename("temp.txt", "final.txt")
+
+	if err != nil{
+		fmt.Println("error renaming file, ", err)
+		return 
+	}
+	fmt.Println("file Renamed successfully")
+}
+
+func deleteFile(name string){
+
+
+	err := os.Remove(name)
+
+	if err != nil{
+		fmt.Println("file not found")
+		return 
+	}
+	fmt.Println("file deleted successfully")
+}
+
+func main() {
+
+	renamefile()
+	deleteFile("del.txt")
+}
