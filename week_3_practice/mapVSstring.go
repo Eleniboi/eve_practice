0b3475941a67eab1bb0274933d86953549c31f44
somafu <omafuoloche4@gmail.com>
2026-05-14 05:44:59 +0100

    map VS string

---
+++ week_3_practice/mapVSstring.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var text string
+
+	fmt.Print("Enter sentence: ")
+
+	scanner.Scan()
+
+	text = scanner.Text()
+
+	splitInput := strings.Split(text, " ")
+
+	var m = make(map[string]int)
+
+	for _, ch := range splitInput {
+
+		for i := range ch {
+
+			if strings.ContainsAny(ch, ":.,?!;") && i == len(ch)-1 {
+
+				ch = ch[:len(ch)-1]
+			}
+		}
+		m[strings.ToLower(ch)]++
+
+	}
+
+	fmt.Println(m)
+}
