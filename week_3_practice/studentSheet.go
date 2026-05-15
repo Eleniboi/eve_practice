bdf1c0264542a4e7fdce4c007a6c024708014f74
somafu <omafuoloche4@gmail.com>
2026-05-15 14:05:23 +0100

    group practice

---
+++ week_3_practice/studentSheet.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func AverageScore(score []float64) (float64, error) {
+
+	var Total float64
+
+	if len(score) == 0 {
+		return 0, errors.New("Average score is empty")
+	}
+	var div int
+
+	for i, value := range score {
+
+		if i < len(score) {
+			Total += value
+		}
+		div = i
+	}
+
+	Ave := Total / float64(div+1) 
+	return Ave, nil
+}
+
+func getGrade(grade float64)string{
+
+	if grade > 100{
+		return fmt.Sprintln("Score is out of range")
+	}
+	if grade >= 70 {
+		return fmt.Sprintln("A")
+	} else if grade >= 60 {
+		return fmt.Sprintln("B")
+	}else if grade >= 50 {
+		return fmt.Sprintln("C")
+	}
+	return fmt.Sprintln("F")
+}
+
+
+func studentReport(Student_name string, scores []float64)string{
+
+	report, err := AverageScore(scores)
+
+	if err != nil{
+		return fmt.Sprintln(err)
+	}
+
+	Final := getGrade(report)
+
+	return fmt.Sprintf("%s | Average %.1f | Grade %s ", Student_name, report, Final)
+}
+//70 and above -> "A"
+//60 - 69 -> "B"
+// 50 - 59 -> "C"
+//50 Below "F"
+//
+func main() {
+
+	// score := []float64{89, 56, 55}
+
+	// result, _ := AverageScore(score)
+	fmt.Println()
+	fmt.Println(studentReport("rtrt", []float64{4,4,33,}))
+	//fmt.Print(getGrade(120))
+}
